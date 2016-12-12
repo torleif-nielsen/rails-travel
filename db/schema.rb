2d0f8bdb129012f592cfc830359fbf7e44cf0970
Torleif Nielsen <torleif@hawaii.edu>
2016-12-11 17:06:39 -1000

    Add offices using rails generate scaffold Office name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210070215) do
+ActiveRecord::Schema.define(version: 20161212024634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "offices", force: :cascade do |t|
-    t.string   "location"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
